0c8a4410ea145882327d5e51d1b76bc19421d088
Tarmity Flassman <tarmity.flassman@gmail.com>
2021-06-01 11:26:11 +1000

    Updates

---
+++ precache-manifest.7c2c0d1066308a647f77dc79e791d2d4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3b9d481a763bee1d8016e1e248a2f1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "4ec1212c71cee032e04a",
+    "url": "/react-portfolio/static/css/2.8163d445.chunk.css"
+  },
+  {
+    "revision": "22387b82b21b254369f4",
+    "url": "/react-portfolio/static/css/main.0a692af4.chunk.css"
+  },
+  {
+    "revision": "4ec1212c71cee032e04a",
+    "url": "/react-portfolio/static/js/2.9fc64bab.chunk.js"
+  },
+  {
+    "revision": "596676716ca1328e2733d607535d6ca7",
+    "url": "/react-portfolio/static/js/2.9fc64bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22387b82b21b254369f4",
+    "url": "/react-portfolio/static/js/main.2a60aec0.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "c03cd45645dfdf2ce81d0a339643787d",
+    "url": "/react-portfolio/static/media/Resume2.c03cd456.pdf"
+  },
+  {
+    "revision": "66d663e93ade9bb9a33947612b7b0a30",
+    "url": "/react-portfolio/static/media/TarmityLogo2.66d663e9.png"
+  },
+  {
+    "revision": "068dbe4e642a65b2870e07fc23a61f37",
+    "url": "/react-portfolio/static/media/cartoonPic.068dbe4e.png"
+  },
+  {
+    "revision": "c7b79c08d3471824f36f68799fdb708d",
+    "url": "/react-portfolio/static/media/email.c7b79c08.png"
+  },
+  {
+    "revision": "b810d391d701c4a13568d8966acea2be",
+    "url": "/react-portfolio/static/media/phone.b810d391.png"
+  }
+]);
