5f20371a105ddd09c6ca14e0123f80a1923af0c4
Tarmity Flassman <tarmity.flassman@gmail.com>
2021-06-01 14:40:27 +1000

    fixed the bug for the download button

--- src/components/Resume/single-page.js
+++ src/components/Resume/single-page.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import "./Resume.css";
-import "./Resume2.pdf";
+import './Resume2.pdf';
 import { } from 'react-bootstrap'
 import { Document, Page, pdfjs } from 'react-pdf'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
